079cb83140393d7cbeaffe4a2451bad198d9ec0b
digvijaybind <69695119+anshu787@users.noreply.github.com>
2023-07-24 22:07:25 +0530

    clock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const fetchTime = async () => {
+      try {
+        const response = await fetch('http://worldtimeapi.org/api/ip');
+        const data = await response.json();
+        // Extract the current time from the response (data.datetime is in ISO format)
+        setCurrentTime(new Date(data.datetime));
+      } catch (error) {
+        console.error('Error fetching time:', error);
+      }
+    };
+
+    // Fetch the time when the component mounts
+    fetchTime();
+
+    // Update the time every second
+    const interval = setInterval(fetchTime, 1000);
+
+    // Clear the interval when the component is unmounted.
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (date) => {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <h2>Current Time Is</h2>
+  <h1>{formatTime(currentTime)}</h1>
+  </>
   );
 }
 
